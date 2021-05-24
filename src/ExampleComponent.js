38b0217190afdf8cbc23ff46289f9f4a0bc0d69b
maxrabin <max.rabin17@gmail.com>
2021-05-24 10:21:27 -0400

    Completed Assignment

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -6,10 +6,15 @@ class ExampleComponent extends Component {
     return (
       <div className="example">
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
     );
   }
 }
+
 
 export default ExampleComponent;
